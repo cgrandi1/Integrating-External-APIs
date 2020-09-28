e625d0db1b990fbc277724bd6d4a857dab161e9d
github username <carltondgrandison@gmail.com>
2020-09-28 13:38:14 -0600

    connecting backend to frontend

--- action/getRequests.js
+++
@@ -1,9 +0,0 @@
-$.getJSON("https://api.sandbox.treasuryprime.com/account/:id/?account_id=YOUR_ACCESS_ID", function(data){
-    console.log(data)
-
-    $.each(data, function(index, value){
-        console.log(value)
-        var name = value.user.name;
-        var status = value.user.status
-    })
-})
